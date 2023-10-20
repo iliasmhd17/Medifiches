95e443c15bf42d9582db90a133c172a99578bd87
Yahya OUAMAR <54915@etu.he2b.be>
2023-10-20 07:18:17 +0200

    ajusted the footer and added an example

--- MediFiches/resources/views/animateur/fiche_medicale.blade.php
+++ MediFiches/resources/views/animateur/fiche_medicale.blade.php
@@ -1,18 +1,18 @@
 @extends('template')
 @section('title', " - Fiche Médicale")
 @section('content')
-<div class="pbteam-container pbteam-padding-32" id="fiche-medicale">
+<div class="pbteam-container pbteam-padding-32" id="fiche-medicale" style="margin-bottom : 2.2em;">
   <h3 class="pbteam-border-bottom pbteam-border-light-grey pbteam-padding-16">Fiche Médicale</h3>
-  <p><strong>Nom de l'enfant: </strong> <span>{{$values->childLastName}}</span></p>
-  <p><strong>Prénom de l'enfant: </strong><span></span>{{$values->childFirstName}}</p>
-  <p><strong>Date de naissance:</strong></p>
-  <p><strong>Peut nager: </strong></p>
-  <p><strong>Médicaments: </strong></p>
-  <p><strong>Quantité de médicaments: </strong></p>
-  <p><strong>Indiquez la fréquance: </strong></p>
-  <p><strong>Allergies: </strong><span>{{$values->allergies}}</span></p>
-  <p><strong>Conséquances des allergies: </strong></p>
-  <p><strong>Vaccination tetanos: </strong></p>
-  <p><strong>Adresse:</strong></p>
+  <p><strong>Nom de l'enfant: </strong> <span>Leblanc</span></p>
+  <p><strong>Prénom de l'enfant: </strong><span></span>Juste</p>
+  <p><strong>Date de naissance: </strong>31/10/1997</p>
+  <p><strong>Peut nager: </strong>Oui</p>
+  <p><strong>Médicaments: </strong>Sediplus Sleep</p>
+  <p><strong>Quantité de médicaments: </strong>1</p>
+  <p><strong>Indiquez la fréquance: </strong>Chaque nuit</p>
+  <p><strong>Allergies: </strong><span>Aucune</span></p>
+  <p><strong>Conséquances des allergies: </strong>Aucune</p>
+  <p><strong>Vaccination tetanos: </strong>Oui</p>
+  <p><strong>Adresse: </strong>Rue Royale 67, 1000 Bruxelles</p>
 </div>
 @endsection
