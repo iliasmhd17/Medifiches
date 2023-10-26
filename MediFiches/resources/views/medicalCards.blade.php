286daacc1315e67b59480b42c88ae7c0651a68f0
vivace fouefack <54490@etu.he2b.be>
2023-10-26 12:51:39 +0200

    update all medical record interface

--- MediFiches/resources/views/medicalCards.blade.php
+++ MediFiches/resources/views/medicalCards.blade.php
@@ -12,17 +12,31 @@
 </head>
 
 <body>
-
     @foreach ($data as $row)
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <h5 class="card-title">{{ $row->national_number }}</h5>
-              <h6 class="card-subtitle mb-2 text-muted"> Médecin: {{ $row->medecins }}</h6>
-              <p class="card-text">Allergie: {{ $row->allergies }}</p>
-              <p class="card-text">Consequences: {{ $row->allergies_consequences }}</p>
-              <a href="#" class="card-link">Détail</a>
-            </div>
-          </div>
+
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">{{ $row->national_number }}</h5>
+          <h6 class="card-subtitle mb-2 text-muted"> Médecin: {{ $row->medecins }}</h6>
+          <p class="card-text">Allergie: {{ $row->allergies }}</p>
+          <p class="card-text">Consequences: {{ $row->allergies_consequences }}</p>
+          <a href="#" class="card-link">Détail</a>
+        </div>
+      </div>
+
+      <div class="card text-center">
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Special title treatment</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  <div class="card-footer text-muted">
+    2 days ago
+  </div>
+</div>
     @endforeach
 </body>
 
