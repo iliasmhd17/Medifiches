450d4704142ace0d18c3d8b48a233477be00e00e
Yahya OUAMAR <54915@etu.he2b.be>
2023-11-13 18:01:56 +0100

    changed some details in the medical record

--- MediFiches/resources/views/medicalCardsDetails.blade.php
+++ MediFiches/resources/views/medicalCardsDetails.blade.php
@@ -3,12 +3,11 @@
 @section('content')
     
     <div class="postition-relative">
-        <img src="{{ asset('images/medi-banner.png') }}" id="banner" alt="Banner" class="img-fluid" style="width: 100%; height: auto;">
         <div class="container mt-5 position-absolute bg-white" id="consult">
             <div class="d-flex justify-content-between align-items-center mb-3">
 
             @foreach ($data as $row)
-            <br><br><br><br><h3 class="pb-3">Fiche médicale <strong> {{ $row->national_number }}</strong></h3>
+            <br><br><br><br><h3 class="pb-3">Fiche médicale de <strong> {{ $row->last_name }} {{ $row->first_name }}</strong></h3>
                 <form method="POST" action="{{ route('generate-pdf') }}" class="ms-auto">
 
                     <input type="text" name="national_number" value="{{$row->national_number}}" hidden>
@@ -29,6 +28,8 @@
             <ul class="list-group list-group-flush">
               
               <li class="list-group-item"><strong>Numéro national : </strong>{{ $row->national_number }}</li>
+              <li class="list-group-item"><strong>Nom : </strong>{{$row->last_name }}</li>
+              <li class="list-group-item"><strong>Prénom : </strong>{{$row->first_name }}</li>
               <li class="list-group-item"><strong>Médecin : </strong> {{ $row->medecins }}</li>
               <li class="list-group-item"><strong>Allergie:</strong> {{ $row->allergies }}</li>
               <li class="list-group-item"><strong>Consequences:</strong> {{ $row->allergies_consequences }}</li>
@@ -37,7 +38,7 @@
               <li class="list-group-item"><strong>Date De Naissance:</strong> {{ $row->birth_date }}</li>
               <li class="list-group-item"><strong>Note Extra: </strong>{{ $row->additional_infos }}</li>
               <li class="list-group-item"><strong>Rue:</strong> {{ $row->street }}</li>
-              <li class="list-group-item"><strong>numéro de maison:</strong> {{ $row->no }}</li>
+              <li class="list-group-item"><strong>Numéro de maison:</strong> {{ $row->no }}</li>
               <li class="list-group-item"><strong>Ville:</strong> {{ $row->city }}</li>
               <li class="list-group-item"><strong>Code Postal:</strong> {{ $row->postal_code }}</li>
               <li class="list-group-item"><strong>Pays:</strong> {{ $row->country }}</li>
