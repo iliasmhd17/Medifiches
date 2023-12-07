7e4a97e23df6ba3f101a6d88dfa8e634312455a1
LordBaljeet <lordbaljeet24@gmail.com>
2023-12-07 16:14:22 +0100

    visual fixed

--- MediFiches/resources/views/medicalCards.blade.php
+++ MediFiches/resources/views/medicalCards.blade.php
@@ -11,31 +11,29 @@
 
     </x-slot>
 
-    <div
-        style="background-image: url('{{ asset('images/medi-banner.png') }}'); background-size: cover; background-position: center; height: 100vh; position: relative; background-attachment: fixed;">
-        <div class="container-fluid mt-5 position-absolute text-white d-flex align-items-center justify-content-center"
-            id="consult">
+    <div style="background-image: url('{{ asset('images/medi-banner.png') }}'); background-size: cover; background-position: center; height: 100vh; position: relative; background-attachment: fixed;">
+        <div class="container-fluid mt-5 position-absolute text-white d-flex align-items-center justify-content-center" id="consult">
             <div class="row row-cols-1 row-cols-md-3 w-100">
                 @foreach ($data as $row)
-                    <div class="col-md-4 mb-4">
-                        <div class="card border-secondary h-100 w-100">
-                            <div class="card-header" style="color: #000; font-size: 1.25rem; font-weight: bold;">
-                                <h5 class="card-title">{{ $row->last_name . ' ' . $row->first_name }}</h5>
-                            </div>
-                            <div class="card-body text-secondary">
-                                <div class="row">
-                                    <div class="col-md-6">
-                                        <p class="card-text mb-2 text-left">Numéro-National:</p>
-                                        <p class="card-text mb-2 text-left">Médecin:</p>
-                                        <p class="card-text mb-2 text-left">Allergie:</p>
-                                        <p class="card-text mb-2 text-left">Dernière mise à jour:</p>
-                                    </div>
-                                    <div class="col-md-6">
-                                        <p class="card-text mb-2">{{ $row->national_number }}</p>
-                                        <p class="card-text mb-2">{{ $row->doctor  }}</p>
-                                        <p class="card-text mb-2">{{ $row->allergies }}</p>
-                                        <p class="card-text mb-2">{{ Carbon\Carbon::parse($row->updated_at)->isoFormat('D MMMM YYYY', 'fr')  }}</p>
-                                    </div>
+                <div class="col-md-4 mb-4">
+                    <div class="card border-secondary h-100 w-100">
+                        <div class="card-header" style="color: #000; font-size: 1.25rem; font-weight: bold;">
+                            <h5 class="card-title">{{ $row->last_name . ' ' . $row->first_name }}</h5>
+                        </div>
+                        <div class="card-body text-secondary">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <p class="card-text mb-2 text-left">Numéro-National:</p>
+                                    <p class="card-text mb-2 text-left">Médecin:</p>
+                                    <p class="card-text mb-2 text-left">Allergie:</p>
+                                    <p class="card-text mb-2 text-left">Dernière mise à jour:</p>
+                                </div>
+                                <div class="col-md-6">
+                                    <p class="card-text mb-2">{{ $row->national_number }}</p>
+                                    <p class="card-text mb-2">{{ $row->doctor  }}</p>
+                                    <p class="card-text mb-2">{{ $row->allergies }}</p>
+                                    <p class="card-text mb-2">{{ Carbon\Carbon::parse($row->updated_at)->isoFormat('D MMMM YYYY', 'fr')  }}</p>
+                                </div>
 
 
                                 <div class="flex">
@@ -46,16 +44,15 @@
                                     </form>
                                     <form action="{{ route('delete_record') }}" method="post">
                                         @csrf
-                                        <input type="text" name="national_number" id="national_number"
-                                            value="{{ $row->national_number }}" hidden>
+                                        <input type="text" name="national_number" id="national_number" value="{{ $row->national_number }}" hidden>
                                         <x-button type="submit">Delete</x-button>
                                     </form>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </div>
                 @endforeach
             </div>
         </div>
-    </div>
 </x-app-layout>
